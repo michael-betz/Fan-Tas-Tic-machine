f403e666b9d2d03787a211bfa6dc154e93d052d6
yetifrisstlama <michibetz@gmail.com>
2019-04-23 22:47:27 -0700

    add 4 score wheels and sound

--- modes/bonus10/code/bonus10.py
+++ modes/bonus10/code/bonus10.py
@@ -1,7 +1,8 @@
 from mpf.core.mode import Mode
-from numpy import array, zeros, ones, uint8, roll, arange, linspace
+from numpy import array, zeros, uint8, roll
 from numpy.random import randint
 from colorsys import hsv_to_rgb
+
 
 class Bonus10(Mode):
     """ controls the 10 central white playfield cutouts with rad animations """
@@ -11,28 +12,36 @@ class Bonus10(Mode):
         self.player.bonus10HitCount = 0
         self.player.bonus10Worth = 1000
         self.add_mode_event_handler('shot_adv_bonus_hit', self.hit)
-        self.add_mode_event_handler('bonus10_bonus',      self.decrementAniStart )
+        self.add_mode_event_handler('bonus10_bonus', self.decrementAniStart)
         self.add_mode_event_handler('roulette_adv_bonus_max', self.hitMax)
-        self.bls = [ getattr( self.machine.lights, "l_bonus{}".format(i) ) for i in range(10) ]
-        self.cs = zeros(30,dtype=uint8).reshape((3,10))
+        self.bls = [
+            getattr(
+                self.machine.lights,
+                "l_bonus{}".format(i)
+            ) for i in range(10)
+        ]
+        self.cs = zeros(30, dtype=uint8).reshape((3, 10))
         self.colorSet()
 
     def colorSet(self):
-        ''' set the 10 RGB leds to the values of the `cs` array of [3:10] brightness values '''
+        '''
+        set the 10 RGB leds to the values of the `cs` array
+        of [3:10] brightness values
+        '''
         for i, bl in enumerate(self.bls):
-            bl.color( self.cs[:,i].tolist() )
+            bl.color(self.cs[:, i].tolist())
 
     def mode_stop(self, **kwargs):
         self.cs[:] = 0
         self.colorSet()
-    
-    def hitMax( self, **kwargs ):
+
+    def hitMax(self, **kwargs):
         if self.player.bonus10HitCount < 10:
             self.hit()
             # recursively call again in 0.1 seconds
-            self.delay.reset(200, self.hitMax, "bonus10AniDelay" )
+            self.delay.reset(200, self.hitMax, "bonus10AniDelay")
 
-    def hit( self, **kwargs ):
+    def hit(self, **kwargs):
         self.player.bonus10HitCount += 1
         hc = self.player.bonus10HitCount
         if hc <= 4:
@@ -43,7 +52,7 @@ class Bonus10(Mode):
             # Green channel, 150 --> 255
             # tmp[1,:] = linspace(150,255,hc)
             # self.cs[:,:hc] = randint(0,4,(3,hc%10))*85
-            cval = randint(0,4,(3,1))*85
+            cval = randint(0, 4, (3, 1)) * 85
         elif hc == 11:
             # Start mega bonus and killer rainbow animation
             self.player.bonus10Worth += 1000
@@ -53,33 +62,36 @@ class Bonus10(Mode):
         else:
             self.machine.events.post("superMegaHyperBonus")
             return
-        self.cs[:,:hc] = cval
+        self.cs[:, :hc] = cval
         self.colorSet()
-    
-    def decrementAniStart( self, **kwargs ):
-        # kwargs = {'hits': 2, 'score': 2000, 'bonus_score': 2000, 'mode': <Mode.bonus10>}
-        self.decrementAni( curLed=min((kwargs["hits"]-1),9) )
 
-    def decrementAni( self, **kwargs ):
+    def decrementAniStart(self, **kwargs):
+        # kwargs = {
+        #    'hits': 2,
+        #    'score': 2000,
+        #    'bonus_score': 2000,
+        #    'mode': <Mode.bonus10>
+        # }
+        self.decrementAni(curLed=min((kwargs["hits"] - 1), 9))
+
+    def decrementAni(self, **kwargs):
         curLed = kwargs["curLed"]
         if curLed >= 0:
-            self.cs[:,curLed] = 0
+            self.cs[:, curLed] = 0
             self.colorSet()
         if curLed > 0:
             curLed -= 1
             # recursively call again in 0.1 seconds
-            self.delay.reset(200, self.decrementAni, "bonus10AniDelay", curLed=curLed )
+            self.delay.reset(
+                200, self.decrementAni, "bonus10AniDelay", curLed=curLed
+            )
 
-    def ultraAniStart( self ):
+    def ultraAniStart(self):
         for i in range(10):
-            self.cs[:,i] = array( hsv_to_rgb( i/20, 1, 1 ) ) * 255
+            self.cs[:, i] = array(hsv_to_rgb(i / 20, 1, 1)) * 255
         self.ultraAni()
 
-    def ultraAni( self ):
-        self.cs = roll( self.cs, 1 )
+    def ultraAni(self):
+        self.cs = roll(self.cs, 1)
         self.colorSet()
-        self.delay.reset(100, self.ultraAni, "bonus10AniDelay" )
-
-
-
-        
+        self.delay.reset(100, self.ultraAni, "bonus10AniDelay")
--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if [ -z "$STY" ]; then exec screen -dm -S mpf /bin/bash "$0"; fi
 GAME="/home/sysop/Fan-Tas-Tic-machine"
+# mpf will start as root and drop privilege
 mpf $GAME -t &
 sleep 5
+# mpf mc will start as sysop
 sudo -u sysop mpf mc $GAME -t
