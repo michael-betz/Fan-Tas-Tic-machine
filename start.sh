a7337d6c7fb225f4b837530da0a476da6737b28e
yetifrisstlama <michibetz@gmail.com>
2019-04-27 14:10:51 -0700

    start.sh: fixed tempfile permission problem

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ -z "$STY" ]; then exec screen -dm -S mpf /bin/bash "$0"; fi
 GAME="/home/sysop/Fan-Tas-Tic-machine"
 # mpf will start as root and drop privilege
-mpf $GAME -t &
+mpf $GAME -At &
 sleep 5
 # mpf mc will start as sysop
-sudo -u sysop mpf mc $GAME -t
+sudo -u sysop mpf mc $GAME -At
