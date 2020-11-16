06d22fcb4717aa2f8ec6e8c66f94d785b88a4f75
Michael Betz <michibetz@gmail.com>
2020-11-15 18:24:16 -0800

    start.sh: run in production mode

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+# run this script as root or with sudo
+# it will start a screen session, which you can connect to with:
+# sudo screen -r mpf
+
 if [ -z "$STY" ]; then exec screen -dm -S mpf /bin/bash "$0"; fi
+
 GAME="/home/sysop/Fan-Tas-Tic-machine"
-# mpf will start as root and drop privilege
-/home/sysop/.pyenv/shims/mpf $GAME -t &
-sleep 5
-# mpf mc will start as sysop
-sudo -u sysop /home/sysop/.pyenv/shims/mpf mc $GAME -t
+
+while true
+do
+	# mpf needs to start as root to access the LED DMD
+	/home/sysop/.pyenv/shims/mpf both $GAME -Pt
+	echo "MPF EXIT :(  restarting in 5 s ..."
+	sleep 5
+done
